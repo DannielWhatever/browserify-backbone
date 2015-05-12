d1c0f4775ef0bbc5fef19e04409ae8ded89d4338
Danniel <dannielgutierrez90@gmail.com>
2015-05-11 22:45:17 -0300

    use port for cloud

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 var express = require('express');
 var app = express();
 
-app.set(process.env.PORT || 5000);
+app.set('port',(process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.listen(app.get('port'), function() {
